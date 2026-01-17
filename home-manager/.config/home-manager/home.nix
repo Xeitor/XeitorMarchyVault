cbb3196a25f9bfeb9f8799e1054c3c608579efb5
Ezequiel Salas <salaseze@gmail.com>
2026-01-17 18:18:56 -0300

    pgadmin to nix

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -22,6 +22,7 @@
   # environment.
   home.packages = [
     pkgs.discord
+    pkgs.pgadmin4
 
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
